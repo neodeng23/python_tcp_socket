a1a6904255941acd7a5d67e5cd951d31f80aad46
Neo_Deng <863494228@qq.com>
2021-05-10 20:41:28 +0800

    # update server demo

--- server.py
+++ server.py
@@ -1,17 +1,31 @@
-#!/usr/bin/env python
-# 服务器端
-import socket
+#!/usr/bin/python3
+# 文件名：server.py
 
-host = "localhost"
-port = 10000
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen(5)
-while 1:
-    sock, addr = s.accept()
-    print("got connection form " + sock.getpeername())
-    data = sock.recv(1024)
-    if not data:
-        break
-    else:
-        print(data)
+# 导入 socket、sys 模块
+import socket
+import sys
+
+# 创建 socket 对象
+serversocket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+
+# 获取本地主机名
+host = socket.gethostname()
+
+port = 9999
+
+# 绑定端口号
+serversocket.bind((host, port))
+
+# 设置最大连接数，超过后排队
+serversocket.listen(5)
+
+while True:
+    # 建立客户端连接
+    clientsocket, addr = serversocket.accept()
+
+    print("连接地址: %s" % str(addr))
+
+    msg = '欢迎访问菜鸟教程！' + "\r\n"
+    clientsocket.send(msg.encode('utf-8'))
+    clientsocket.close()
