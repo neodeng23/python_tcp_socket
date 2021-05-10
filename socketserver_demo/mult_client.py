e2bacebff697b1a6e855fec1880715a3202f31a1
neo_deng <863494228@qq.com>
2021-05-10 21:09:23 +0800

    # update mult

--- socketserver_demo/mult_client.py
+++ socketserver_demo/mult_client.py
@@ -1,17 +1,40 @@
-# -*- coding: UTF-8 -*-
-
 import socket
 
+flag = True
+
+# 生成socket对象
 client = socket.socket()
 
+# 链接要链接的ip和port（端口）
 client.connect(('localhost', 9999))
 
-while True:
+# while循环
+while flag:
 
-    msg = input('>>:').strip()
-    if not msg:
+    # 获得用户输入
+    msg = input("Enter your message('q' for quit):").strip()
+
+    # 判断是否为空
+    if len(msg) == 0:
+        print("Message can't be empty")
         continue
+
+    # 发送数据
+    client.send(msg.encode())
+
+    # 判断是否为'q'
+    if msg != 'q':
+
+        # 接收数据
+        data = client.recv(1024)
+
+        # 打印接收到的数据
+        print(data)
+
     else:
-        client.send(msg.encode('utf-8'))
-        upData = client.recv(1024)
-        print(upData.decode())
+        # 条件为False
+        flag = False
+
+# 关闭socket链接
+client.close()
+print('Connection is broken')
