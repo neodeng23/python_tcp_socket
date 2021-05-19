4ee2f770aa3e5961dd90ed61464db2d256d2c3c6
Neo_Deng <863494228@qq.com>
2021-05-19 13:50:06 +0800

    # update client socket

---
+++ DUT_test_software/client_socket.py
@@ -0,0 +1,27 @@
+import socket
+import time
+from logger import Logger
+
+
+def SendAndRev(msg, host, port):
+    now_time = time.strftime('%Y%m%d')
+    log_name = now_time + ".log"
+    log = Logger(log_name, level='debug')
+    client = socket.socket()
+    log.logger.info('begin try to connect to host: ' + host + ":" + str(port))
+    try:
+        client.connect((host, port))
+        log.logger.info('Success to connect to host: ' + host + ":" + str(port))
+        client.send(msg.encode())
+        data = client.recv(1024)
+        log.logger.info('get infomation from  host: ' + host + ":" + str(port) + ":" + data.decode())
+        client.close()
+        return data.decode()
+    except:
+        log.logger.warning('Fail to connect to host: ' + host + ":" + str(port))
+        return "no connect"
+
+
+msg = "aaaa"
+data = SendAndRev(msg, "192.168.231.232", 9999)
+print(data)
